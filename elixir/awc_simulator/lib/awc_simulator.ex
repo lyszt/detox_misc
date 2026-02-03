5639d30fcfcde1ed1eea4062f0a7510fff2b6f53
lyszt <kalliddel@gmail.com>
2026-02-02 22:33:45 -0300

    git init

---
+++ elixir/awc_simulator/lib/awc_simulator.ex
@@ -0,0 +1,18 @@
+defmodule AwcSimulator do
+  @moduledoc """
+  Documentation for `AwcSimulator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AwcSimulator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/awc_simulator/test/awc_simulator_test.exs
@@ -0,0 +1,8 @@
+defmodule AwcSimulatorTest do
+  use ExUnit.Case
+  doctest AwcSimulator
+
+  test "greets the world" do
+    assert AwcSimulator.hello() == :world
+  end
+end
